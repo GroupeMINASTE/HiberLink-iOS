63264417d980caabf5b7d9a27efb58fd90b93162
Nathan Fallet <contact@nathanfallet.me>
2020-05-09 16:00:33 +0200

    Waiting for REST API fix

--- HiberLink/Utils/AppDelegate.swift
+++ HiberLink/Utils/AppDelegate.swift
@@ -25,6 +25,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        // Check that there is an url with correct host
+        if let url = userActivity.webpageURL, url.host == "hiber.link" {
+            // Check if there are get parameters
+            if let query = url.query {
+                // Fetch target link
+                APIRequest("GET", path: "/index.php").with(name: query).execute(String.self) { string, status in
+                    // Check the response
+                    if let string = string, let full = URL(string: string) {
+                        // Add url to history
+                        Database.current.addLink((url.absoluteString, full.absoluteString))
+                        
+                        // Open url
+                        if #available(iOS 10, *) {
+                            UIApplication.shared.open(full)
+                        } else {
+                            UIApplication.shared.openURL(full)
+                        }
+                    }
+                }
+            }
+            
+            // Return
+            return true
+        }
+        return false
+    }
 
 }
 
